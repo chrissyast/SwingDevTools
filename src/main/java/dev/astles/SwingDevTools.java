2767f90fb5486e65cc40b55ff7421fc667a8d999
chrissyast <cjastles@gmail.com>
2026-03-12 17:29:02 +0000

    add middle button functionality

--- src/main/java/dev/astles/SwingDevTools.java
+++ src/main/java/dev/astles/SwingDevTools.java
@@ -5,14 +5,16 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.awt.event.MouseEvent.MOUSE_ENTERED;
-import static java.awt.event.MouseEvent.MOUSE_EXITED;
-
+import static java.awt.event.FocusEvent.FOCUS_LOST;
+import static java.awt.event.MouseEvent.*;
 
 
 public class SwingDevTools {
@@ -22,14 +24,24 @@ public class SwingDevTools {
     private static final Map<JComponent, Border> originalBorders = new HashMap<>();
     private static final Map<String, Color> assignedColours = new HashMap<>();
     private static boolean isControlPressed = false;
+    private static JComponent topNonSwingComponent = null;
+
+    private static String project;
 
     public static void initialise() {
+        initialise("");
+    }
+    public static void initialise(String projectName) {
+        project = projectName;
         Toolkit.getDefaultToolkit().addAWTEventListener(e -> {
             if (e.getID() == MOUSE_ENTERED) {
                 handleMouseEntered(e);
             }
             if (e.getID() == MOUSE_EXITED) {
                 handleMouseExit(e);
+            }
+            if (e.getID() == MOUSE_CLICKED && ((MouseEvent) e).getButton() == 2 && isControlPressed && !project.isBlank()) {
+                handleMiddleButton(e);
             }
         }, AWTEvent.MOUSE_EVENT_MASK);
         Toolkit.getDefaultToolkit().addAWTEventListener(e -> {
@@ -52,8 +64,12 @@ public class SwingDevTools {
 
     private static String getComponentPath(JComponent comp) {
         String text = comp.getClass().getSimpleName();
-        if (!comp.getClass().getPackageName().contains("javax.swing")) {
+        boolean isSwing = comp.getClass().getPackageName().contains("javax.swing");
+        if (!isSwing) {
             text = "<span style='color: red'>" + text + "</span>";
+            if (topNonSwingComponent == null) {
+                topNonSwingComponent = comp;
+            }
         }
         if (!assignedColours.containsKey(comp.getClass().getSimpleName())) {
             assignedColours.put(comp.getClass().getSimpleName(), colours.get(assignedColours.size() % colours.size()));
@@ -112,6 +128,7 @@ public class SwingDevTools {
     }
 
     private static void handleMouseExit(AWTEvent e) {
+        topNonSwingComponent = null;
         if (!isControlPressed) {
             clearAllBorders();
             return;
@@ -129,5 +146,20 @@ public class SwingDevTools {
         }
         setComponentBorder((JComponent) comp);
         overlay.setVisible(false);
+    }
+
+    private static void handleMiddleButton(AWTEvent e) {
+        String componentPath = topNonSwingComponent.getClass().getName();
+        componentPath = componentPath.replace(".", "%2f");
+        String uri = String.format("jetbrains://idea/navigate/reference?project=%s&path=%s", project, componentPath);
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URI(uri));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            } catch (URISyntaxException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
